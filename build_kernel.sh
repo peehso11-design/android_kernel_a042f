a0cd6d4f6204373324dde5943284d3c82e376caa
peehso11-design <peehso11@gmail.com>
2026-05-19 09:49:24 +0300

    Fix build script - use single-quoted heredoc to prevent PowerShell expansion

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,62 +1,62 @@
 #!/bin/bash
 export ARCH=arm64
-export RDIR="C:\Users\Admin\Desktop\android"
+export RDIR="$(pwd)"
 export KBUILD_BUILD_USER="ksu-build"
 
 #init ksu next
 git submodule init && git submodule update
 
 #export toolchain paths
-export BUILD_CROSS_COMPILE="/toolchains/arm-gnu-toolchain-14.2.rel1-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-"
-export BUILD_CC="/toolchains/clang-r383902/bin/clang"
+export BUILD_CROSS_COMPILE="${RDIR}/toolchains/arm-gnu-toolchain-14.2.rel1-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-"
+export BUILD_CC="${RDIR}/toolchains/clang-r383902/bin/clang"
 
 #output dir
-if [ ! -d "/out" ]; then
-    mkdir -p "/out"
+if [ ! -d "${RDIR}/out" ]; then
+    mkdir -p "${RDIR}/out"
 fi
 
 #build dir
-if [ ! -d "/build" ]; then
-    mkdir -p "/build"
+if [ ! -d "${RDIR}/build" ]; then
+    mkdir -p "${RDIR}/build"
 else
-    rm -rf "/build" && mkdir -p "/build"
+    rm -rf "${RDIR}/build" && mkdir -p "${RDIR}/build"
 fi
 
 #build options
 export ARGS="
--C C:\Users\Admin\Desktop\android \
-O=C:\Users\Admin\Desktop\android/out \
--j \
+-C $(pwd) \
+O=$(pwd)/out \
+-j$(nproc) \
 ARCH=arm64 \
-CROSS_COMPILE= \
-CC= \
+CROSS_COMPILE=${BUILD_CROSS_COMPILE} \
+CC=${BUILD_CC} \
 CLANG_TRIPLE=aarch64-linux-gnu- \
 KCFLAGS=-w \
 CONFIG_SECTION_MISMATCH_WARN_ONLY=y \
 "
 
-#build kernel image only
+#build kernel image
 build_kernel(){
-    make  clean && make  mrproper
-    make  a04e_defconfig custom.config
-    make  || exit 1
-    cp out/arch/arm64/boot/Image.gz /build/Image.gz
-    cp out/arch/arm64/boot/Image /build/Image
+    make ${ARGS} clean && make ${ARGS} mrproper
+    make ${ARGS} a04e_defconfig custom.config
+    make ${ARGS} || exit 1
+    cp out/arch/arm64/boot/Image.gz ${RDIR}/build/Image.gz
+    cp out/arch/arm64/boot/Image ${RDIR}/build/Image
 }
 
-#build boot.img with AIK (fallback)
+#build boot.img with AIK (fallback, produces incomplete boot.img)
 build_boot() {    
-    rm -f /AIK-Linux/split_img/boot.img-kernel /AIK-Linux/boot.img
-    cp "/out/arch/arm64/boot/Image.gz" /AIK-Linux/split_img/boot.img-kernel
-    mkdir -p /AIK-Linux/ramdisk/{debug_ramdisk,dev,metadata,mnt,proc,second_stage_resources,sys}
-    cd /AIK-Linux && ./repackimg.sh --nosudo && mv image-new.img /build/boot.img
+    rm -f ${RDIR}/AIK-Linux/split_img/boot.img-kernel ${RDIR}/AIK-Linux/boot.img
+    cp "${RDIR}/out/arch/arm64/boot/Image.gz" ${RDIR}/AIK-Linux/split_img/boot.img-kernel
+    mkdir -p ${RDIR}/AIK-Linux/ramdisk/{debug_ramdisk,dev,metadata,mnt,proc,second_stage_resources,sys}
+    cd ${RDIR}/AIK-Linux && ./repackimg.sh --nosudo && mv image-new.img ${RDIR}/build/boot.img
 }
 
-#build odin flashable tar
+#build odin flashable tar with Image.gz + Image + boot.img
 build_tar(){
-    cd /build
-    tar -cvf "KernelSU-Next-SM-A042F.tar" Image.gz Image boot.img && rm boot.img
-    echo -e "\n[i] Build Finished..!\n" && cd 
+    cd ${RDIR}/build
+    tar -cvf "KernelSU-Next-SM-A042F.tar" Image.gz Image boot.img
+    echo -e "\n[i] Build Finished..!\n" && cd ${RDIR}
 }
 
 build_kernel
